4111841ccbffecbaea477d7215afc907d075cd1c
ttoomas <folprecht.to@gmail.com>
2024-04-05 09:59:30 +0200

    feat: primereact initialized

---
+++ src/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+
+export const Layout = ({ children }) => {
+  return (
+    <div>
+      <header>
+        {/* Header content goes here */}
+      </header>
+
+      <main>
+        {children}
+      </main>
+
+      <footer>
+        {/* Footer content goes here */}
+      </footer>
+    </div>
+  );
+};
