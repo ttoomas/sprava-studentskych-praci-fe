50ed8a92e975540466d2aecf10f76b65fe6dcbf4
ttoomas <folprecht.to@gmail.com>
2024-05-03 08:01:49 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { HomePage } from "./Pages/Home";
 import { Layout } from "./Layout";
 import UserCreateForm from "./Pages/UserCreateForm/UserCreateForm";
+import LoginForm from "./Pages/LoginForm/LoginForm"
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
     <Layout>
       <Routes>
       <Route path="/" element={<HomePage />} />
-        <Route path="/registrace" element={<UserCreateForm />} />
+        <Route path="/prihlaseni" element={<UserCreateForm />} />
+        <Route path="/registrace" element={<LoginForm />} />
       </Routes>
     </Layout>
   </Router>
--- src/Pages/UserCreateForm/UserCreateForm.jsx
+++ src/Pages/UserCreateForm/UserCreateForm.jsx
@@ -5,6 +5,7 @@ import { InputText } from 'primereact/inputtext';
 import { SelectButton } from 'primereact/selectbutton';
 import { Password } from 'primereact/password';
 import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
 
 
 export default function UserCreateForm() {
@@ -37,21 +38,38 @@ export default function UserCreateForm() {
 
     return (
         <>
-        <h1>user create form</h1>
-        <form>
-        <InputText placeholder="Jméno" name="name" onChange={e => handleChange(e)}/>
-        <Password   placeholder="Heslo" name="password"onChange={e => handleChange(e)} feedback={false} tabIndex={1} />
-        <InputText placeholder="Třída" name="class_name" onChange={e => handleChange(e)}/>
-        <SelectButton name="role" options={["Student", "Učitel"]} value={formData?.role||"Student"} onChange={e => handleChange(e)} />
-        <InputText placeholder="Téma" name="theme" onChange={e => handleChange(e)}/>
-        <InputText placeholder="Obor" name="field" onChange={e => handleChange(e)}/>
-         
-          
-           
-        <Button label="Vytvořit uživatele" />
-               
-        </form>
-        <Link to= {"/"}>go back</Link>
+        <h1>Create new user</h1>
+        <div className="p-grid p-justify-center p-align-center" style={{height: '100vh'}}>
+            <div className="p-col-12 p-md-6 p-lg-4">
+                <Card>
+                    <form onSubmit={handlePost} method="POST">
+                        <div className="p-field">
+                            <InputText placeholder="Jméno" name="name" onChange={e => handleChange(e)}/>
+                        </div>
+                        <div className="p-field">
+                            <Password   placeholder="Heslo" name="password"onChange={e => handleChange(e)} feedback={false} tabIndex={1} />
+                        </div>
+                        <div className="p-field">
+                            <InputText placeholder="Třída" name="class_name" onChange={e => handleChange(e)}/>
+                        </div>
+                        <div className="p-field">
+                            <SelectButton name="role" options={["Student", "Učitel"]} value={formData?.role||"Student"} onChange={e => handleChange(e)} />
+                        </div>
+                        <div className="p-field">
+                            <InputText placeholder="Téma" name="theme" onChange={e => handleChange(e)}/>
+                        </div>
+                        <div className="p-field">
+                            <InputText placeholder="Obor" name="field" onChange={e => handleChange(e)}/>
+                        </div>
+
+                        <Button label="Vytvořit uživatele" />
+                        
+                    </form>
+
+                    <Link to= {"/"}>go back</Link>
+                </Card>
+                </div>
+            </div>
         </>
     )
 }
