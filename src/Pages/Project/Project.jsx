b0283fae9fcadf371f6eead2e0515c9a91951a7b
ttoomas <folprecht.to@gmail.com>
2024-06-20 21:38:25 +0200

    refactor: project

--- src/Pages/Project/Project.jsx
+++ src/Pages/Project/Project.jsx
@@ -1,18 +1,16 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Button } from 'primereact/button';
-import { ToggleButton } from 'primereact/togglebutton';
-import "./TableTheme.css";
-import { Fieldset } from 'primereact/fieldset';
+import { Button } from "primereact/button";
+import "./Project.css";
+import { Fieldset } from "primereact/fieldset";
 import { getProjectById } from "../../models/Project";
 import { formatDate } from "../../Helpers/DateFormat/DateFormat";
-
 
 export default function TableTheme() {
     const { id } = useParams();
     const [project, setProject] = useState();
-    
-    async function getProject(){
+
+    async function getProject() {
         const projectData = await getProjectById(id);
 
         setProject(projectData.data.project);
@@ -20,48 +18,58 @@ export default function TableTheme() {
 
     useEffect(() => {
         getProject();
-    }, [])
-    
-    
-    
+    }, []);
+
     return (
-        <>
+        <div className="project">
+            <h1 className="projNadpis">Podrobnosti projektu</h1>
+            <br />
 
+            <div className="project__container">
+                <Fieldset legend="Name">{project && project.name}</Fieldset>
+                <Fieldset legend="Theme">{project && project.theme}</Fieldset>
+                <Fieldset legend="Student">
+                    {project && project.user.name}
+                </Fieldset>
+                <Fieldset legend="Datum vytvoření">
+                    {project ? formatDate(project.created_at) : ""}
+                </Fieldset>
+                <Fieldset legend="Vedoucí práce">
+                    {project && project.teacher.name}
+                </Fieldset>
+                <Fieldset legend="Popis práce">
+                    {project && project.description}
+                </Fieldset>
+                <Fieldset legend="Obor">{project && project.field}</Fieldset>
 
-<h1 className="projNadpis">Podrobnosti projektu</h1><br />
+                <Fieldset legend="Status">
+                    {project && project.confirm_status ? (
+                        <Button
+                            severity="success"
+                            type="button"
+                            label="Přiřazeno"
+                        />
+                    ) : project && !project.confirm_status && project.user ? (
+                        <Button
+                            severity="warning"
+                            type="button"
+                            label="Čeká na potvrzení"
+                        />
+                    ) : (
+                        <Button
+                            severity="warning"
+                            type="button"
+                            label="Nepřiřazeno"
+                        />
+                    )}
+                </Fieldset>
+            </div>
 
-<div>
-        <Fieldset  legend="Name">{ project && project.name }</Fieldset><br />
-        <Fieldset legend="Theme">{ project && project.theme }</Fieldset><br />
-        <Fieldset legend="Student">{ project && project.user.name }</Fieldset><br />
-        <Fieldset legend="Datum vytvoření">{ project ? formatDate(project.created_at) : "" }</Fieldset><br />
-        <Fieldset legend="Vedoucí práce">{ project && project.teacher.name }</Fieldset><br />
-        <Fieldset legend="Popis práce">{ project && project.description }</Fieldset><br />
-        <Fieldset legend="Obor">{ project && project.field }</Fieldset><br /> 
-        <Fieldset legend="Status">
-            {
-                project && project.confirm_status
-                ? <Button
-                    severity="success"
-                    type="button"
-                    label="Přiřazeno"
-                /> : (project && !project.confirm_status && project.user ? <Button
-                    severity="warning"
-                    type="button"
-                    label="Čeká na potvrzení"
-                /> : <Button
-                    severity="warning"
-                    type="button"
-                    label="Nepřiřazeno"
-                />)
-            }
-        </Fieldset><br />   
-
-</div>
-
-<div className="projeBack"><Link to= {"/updateproject"}><Button label="Upravit projekt" /></Link></div>
-<div className="projeBack"><Link to= {"/projects"}><Button label="Všechny projekty" /></Link></div>
-
-        </>
-    )
+            <div className="projeBack">
+                <Link to={"/projects"}>
+                    <Button label="Zpět" />
+                </Link>
+            </div>
+        </div>
+    );
 }
